6b0441e5153bd71b6da8d7775459b9de775827d5
Massagli <adrielmassagli@gmail.com>
2025-12-19 16:54:32 -0300

    fix img alt for ci build

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -6,7 +6,7 @@ function About(){
     return(
         <section className={styles.about}>
             <div className={styles.image_container}>
-                <div className={styles.cicle}><img src={me}/></div>
+                <div className={styles.cicle}><img src={me} alt="my cartoon"/></div>
                 
             </div>
             <div className={styles.text_container}>
