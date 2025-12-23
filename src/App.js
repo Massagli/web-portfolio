d718522dee98367f7ba9833cb38c2878808c0456
Massagli <adrielmassagli@gmail.com>
2025-12-23 19:53:29 -0300

    adding experience section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/sections/Home';
 import About from './components/sections/About';
 import Skills from './components/sections/Skills';
 import Contact from './components/sections/Contact';
+import Experience from './components/sections/Experience';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Home></Home>
         <About></About>
         <Skills></Skills>
+        <Experience></Experience>
         <Contact></Contact>
         <Footer></Footer>
     </main>
---
+++ src/components/sections/Experience.js
@@ -0,0 +1,77 @@
+import Status from "../ui/Status";
+import styles from "./Experience.module.css";
+
+
+function Experience() {
+  return (
+    <section className={styles.experience}>
+        <Status title="Experiências"></Status>
+        <div className={styles.timeline}>
+            {/* ITEM 1 */}
+            <div className={styles.item}>
+                <div className={styles.left}>
+                    <span className={styles.dot} />
+
+                    <div>
+                    <h3 className={styles.company}>Sumup</h3>
+                    <p className={styles.role}>Planning Intern</p>
+                    <p className={styles.date}>May 2025 - Present · 8 mos</p>
+                    </div>
+                </div>
+
+                <div className={styles.right}>
+                    <ul>
+                    <li>
+                        Creating application interfaces with Figma and modeling databases
+                        in PostgreSQL.
+                    </li>
+                    <li>
+                        Developing a dynamic forecasting model for the Logistics team to
+                        support strategic planning.
+                    </li>
+                    <li>
+                        Designing and planning a new application to support the Sales
+                        team, including PRD and RFC documentation, process mapping with
+                        Service Blueprint and architecture modeling with C4 Model.
+                    </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className={styles.divider} />
+
+            {/* ITEM 2 */}
+            <div className={styles.item}>
+            <div className={styles.left}>
+                <span className={styles.dot} />
+
+                <div>
+                <p className={styles.role}>Apprenticeship - Sales & Growth</p>
+                <p className={styles.date}>Jul 2024 - May 2025 · 11 mos</p>
+                </div>
+            </div>
+
+            <div className={styles.right}>
+                <ul>
+                <li>
+                    Updating and monitoring KPIs to support data-driven
+                    decision-making.
+                </li>
+                <li>
+                    Creating, maintaining, and optimizing SQL queries for data
+                    extraction and analysis.
+                </li>
+                <li>
+                    Automating manual processes and operational routines using Python
+                    to improve efficiency.
+                </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+
+export default Experience;
